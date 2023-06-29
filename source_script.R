2a4b1a4eab6fa2dab70a2d13cbe68e569b16d1be
Kate Miller <kathryn_miller@nps.gov>
2023-06-29 13:41:45 -0400

    updated for 2023

--- source_script.R
+++ source_script.R
@@ -13,15 +13,16 @@ importData()
 
 # Downgrade Fraxinus to subcanopy species
 VIEWS_NETN$Taxa_NETN$IsCanopyExclusion[VIEWS_NETN$Taxa_NETN$Genus == "Fraxinus"] <- TRUE
+#VIEWS_NETN$Taxa_NETN$IsCanopyExclusion[VIEWS_NETN$Taxa_NETN$Genus == "Fagus"] <- FALSE
 
 # Set parameters
-park = 'MORR'
+park = 'SARA'
 from = 2006
-from_4yr = 2018
-to = 2022
+from_4yr = 2022
+to = 2023
 QAQC = FALSE
 locType = 'all'
-cycle_latest = 4
+cycle_latest = 5
 park_crs = ifelse(park %in% c("ACAD", "MIMA"), 26919, 26918)
 
 args_all = list(park = park, from = from, to = to, QAQC = QAQC, locType = locType)
@@ -29,7 +30,7 @@ args_4yr = list(park = park, from = from_4yr, to = to, QAQC = QAQC, locType = lo
 args_vs = list(park = park, from = from, to = to, QAQC = QAQC, locType = "VS")
 
 # Set up file structure
-path = 'D:/NETN/Monitoring_Projects/Forest_Health/Data_Summaries/'
+path = 'C:/NETN/Monitoring_Projects/Forest_Health/Data_Summaries/'
 new_path = paste0(path, park, "/", as.character(to), "/")
 
 if(!dir.exists(new_path)){dir.create(new_path)}
