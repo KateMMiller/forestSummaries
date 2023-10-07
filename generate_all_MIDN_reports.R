1e4366b08c704b5b00fb48be99c6c1dafa310801
Kate Miller <kathryn_miller@nps.gov>
2023-10-07 13:37:34 -0400

    mapping on error

--- generate_all_MIDN_reports.R
+++ generate_all_MIDN_reports.R
@@ -4,6 +4,7 @@
 library(purrr)
 library(rmarkdown)
 library(knitr)
+library(pagedown) # for chrome_print()
 
 rmd_path <- c("C:/NETN/R_Dev/forestSummaries/")
 midn_params <- read.csv("MIDN_params.csv")
@@ -23,15 +24,27 @@ render_MIDN_reports <- function(park){
            output_dir = outpath,
            output_options = list(self_contained = TRUE))#,
   #encoding = "UTF-8")
-   }
+}
+
+pdf_print <- function(park){
+  report_dir <- paste0("C:/NETN/Monitoring_Projects/Forest_Health/Data_Summaries/", 
+                       park, "/", end_year, "/")
+  report_name <- paste0(park, "_Figures_and_Tables_", format(Sys.time(), "%b_%Y"))
+  chrome_print(input = paste0(report_dir, report_name, ".html"), 
+               output = paste0(report_dir, report_name, ".pdf"),
+               format = 'pdf')
+  cat('Report printed to: ', paste0(report_dir, report_name, ".pdf"))
+}
 
 render_poss <- possibly(render_MIDN_reports, otherwise = "Error")
 
-render_MIDN_reports(park = "COLO")
+#render_MIDN_reports(park = "COLO")
 
 midn_parks <- sort(unique(midn_params$park))
 
-purrr::walk(midn_parks, ~render_MIDN_reports(park = .))
+purrr::walk(midn_parks, ~render_poss(park = .))
             
-purrr::walk(midn_parks[4:11], ~render_poss(park = .))
+#purrr::walk(midn_parks[4:11], ~render_poss(park = .))
 # Failed on COLO in regen_debt_metrics_MIDN.R 221
+
+purrr::walk(midn_parks, ~pdf_print(.))
