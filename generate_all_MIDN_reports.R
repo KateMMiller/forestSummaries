b2e49b349583bdc42026a3641c38055f78b77020
KateMMiller <kathryn_miller@nps.gov>
2024-09-23 10:15:27 -0400

    added fix for chrome_print

--- generate_all_MIDN_reports.R
+++ generate_all_MIDN_reports.R
@@ -6,8 +6,12 @@ library(rmarkdown)
 library(knitr)
 library(pagedown) # for chrome_print()
 
+# 20240923: chrome_print is failing to render a .pdf. Need to install servr package for fix:
+#devtools::install_github('yihui/servr')
+library(servr)
+
 rmd_path <- c("C:/NETN/R_Dev/forestSummaries/")
-out_path <- paste0(rmd_path, "/output/")
+out_path <- paste0(rmd_path, "output/")
 midn_params <- read.csv("MIDN_params.csv")
 report_path <- "C:/NETN/Monitoring_Projects/Forest_Health/Data_Summaries/" #path where source script
 # puts output from data summaries.
@@ -49,7 +53,8 @@ render_MIDN_reports <- function(park){
 }
 
 pdf_print <- function(park){
-  report_dir <- paste0(report_path, park, "/", end_year, "/")
+#  report_dir <- paste0(report_path, park, "/", end_year, "/")
+  report_dir <- paste0(out_path)
   report_name <- paste0(park, "_Figures_and_Tables_", format(Sys.time(), "%b_%Y"))
   chrome_print(input = paste0(out_path, report_name, ".html"), 
                output = paste0(out_path, report_name, ".pdf"),
@@ -68,6 +73,8 @@ midn_parks <- c("APCO", "ASIS", "BOWA", "COLO", "FRSP", "GETT",
                 "GEWA", "HOFU", "PETE", "RICH", "SAHI", "THST", "VAFO")
 purrr::walk(midn_parks, ~render_poss(park = .))
 purrr::walk(midn_parks, ~print_poss(.))
+
+pdf_print("APCO")
 
 # render_poss("SAHI")
 # print_poss("SAHI")
